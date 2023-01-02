14d083e4458ea660a8c8660532bc710c32388f6c
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2023-01-02 17:49:14 +0500

    improve decode speed with `BufReader`

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use std::fs::File;
+use std::io::BufReader;
 mod qoi;
 use image::{RgbImage, RgbaImage};
 use qoi::*;
 
 fn main() {
-    let bytes = File::open("qoi_test_images/wikipedia_008.qoi").unwrap();
+    let file = File::open("wikipedia_008.qoi").unwrap();
+    let mut bytes = BufReader::new(file);
     let (data, desc) = qoi_decode(bytes, None).unwrap();
 
     println!("{desc:?}");
     match desc.channels {
         ChanelMode::Rgb => RgbImage::from_raw(desc.width as u32, desc.height as u32, data)
             .unwrap()
-            .save("wikipedia_008.png")
+            .save("rust.png")
             .unwrap(),
         ChanelMode::Rgba => RgbaImage::from_raw(desc.width as u32, desc.height as u32, data)
             .unwrap()
-            .save("wikipedia_008.png")
+            .save("rust.png")
             .unwrap(),
     };
 }
