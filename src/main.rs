56df6fb206a44315d4c73822ca18ca10de81bcb8
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2023-01-06 00:23:24 +0500

    change if to match to check for None variant of `Path::extension` needs testing against different filenames, and more forgiving cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Write};
 use std::path::PathBuf;
@@ -19,30 +20,34 @@ struct Args {
 
 fn main() {
     let cli = Args::parse();
-    if cli.input.extension().expect("No extension") == "qoi" {
-        let file = File::open(cli.input).expect("cannot open file");
-        let buf = BufReader::new(file);
-        let (pixels, desc) =
-            qoi_decode(buf, Some(ChanelMode::Rgba)).expect("unable to decode qoi image");
-        RgbaImage::from_raw(desc.width as u32, desc.height as u32, pixels)
-            .expect("unable to encode image")
-            .save(&cli.output)
-            .unwrap_or_else(|_| panic!("unable to save image to {:?}", cli.output));
-    } else {
-        let image = image::open(cli.input).expect("your supplied image is not correct");
-        let pixels = image.to_rgba8();
-        let file = File::create(cli.output).expect("cannot create file");
-        let bytes = qoi_encode(
-            &pixels,
-            QoiDescriptor {
-                width: image.width() as usize,
-                height: image.height() as usize,
-                channels: ChanelMode::Rgba,
-                colorspace: Colorspace::Srgb,
-            },
-        )
-        .expect("unable to decode image");
-        let mut buf = BufWriter::new(file);
-        buf.write_all(&bytes).expect("unable to write to file");
+    match cli.input.extension().and_then(OsStr::to_str) {
+        Some("qoi") => {
+            let file = File::open(cli.input).expect("cannot open file");
+            let buf = BufReader::new(file);
+            let (pixels, desc) =
+                qoi_decode(buf, Some(ChanelMode::Rgba)).expect("unable to decode qoi image");
+            RgbaImage::from_raw(desc.width as u32, desc.height as u32, pixels)
+                .expect("unable to encode image")
+                .save(&cli.output)
+                .unwrap_or_else(|_| panic!("unable to save image to {:?}", cli.output));
+        }
+        Some(_) => {
+            let image = image::open(cli.input).expect("your supplied image is not correct");
+            let pixels = image.to_rgba8();
+            let file = File::create(cli.output).expect("cannot create file");
+            let bytes = qoi_encode(
+                &pixels,
+                QoiDescriptor {
+                    width: image.width() as usize,
+                    height: image.height() as usize,
+                    channels: ChanelMode::Rgba,
+                    colorspace: Colorspace::Srgb,
+                },
+            )
+            .expect("unable to decode image");
+            let mut buf = BufWriter::new(file);
+            buf.write_all(&bytes).expect("unable to write to file");
+        }
+        None => panic!("no extension"),
     }
 }
