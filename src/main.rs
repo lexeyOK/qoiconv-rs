e22d8053a0c570e593f0ce7a9dc8319ad4aed024
Alexey Kachanov <35109763+lexeyOK@users.noreply.github.com>
2023-01-06 17:38:26 +0500

    make better cli

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,51 @@
+use clap::{arg, command, Parser};
+use image::RgbaImage;
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufReader, Write};
 use std::path::PathBuf;
 mod qoi;
-use clap::{arg, command, Parser};
-use image::RgbaImage;
 use qoi::*;
 
 #[derive(Parser)]
-#[command(author,version,about,long_about = None)]
+#[command(author,version,about,long_about = None, arg_required_else_help = true)]
 struct Args {
     /// Path to input image file
-    #[arg(short, long)]
     input: PathBuf,
-    /// Path to  qoi output file
-    #[arg(short, long)]
-    output: PathBuf,
+    /// Path to qoi output file
+    #[arg(short)]
+    output: Option<PathBuf>,
 }
 
 fn main() {
     let cli = Args::parse();
     match cli.input.extension().and_then(OsStr::to_str) {
         Some("qoi") => {
-            let file = File::open(cli.input).expect("cannot open file");
+            // open file
+            let file = File::open(&cli.input).expect("cannot open file");
             let buf = BufReader::new(file);
+
+            // decode pixels
             let (pixels, desc) =
                 qoi_decode(buf, Some(ChanelMode::Rgba)).expect("unable to decode qoi image");
+
+            // encode in new file and save it
+            let output = cli.output.unwrap_or(cli.input.with_extension("png"));
             RgbaImage::from_raw(desc.width as u32, desc.height as u32, pixels)
                 .expect("unable to encode image")
-                .save(&cli.output)
-                .unwrap_or_else(|_| panic!("unable to save image to {:?}", cli.output));
+                .save(&output)
+                .unwrap_or_else(|_| panic!("unable to save image to {:?}", &output));
         }
         Some(_) => {
-            let image = image::open(cli.input).expect("your supplied image is not correct");
+            // open and decode image
+            let image = image::open(&cli.input).expect("your supplied image is not correct");
             let pixels = image.to_rgba8();
-            let mut file = File::create(cli.output).expect("cannot create file");
+
+            // create file for encoded qoi image
+            let output = cli.output.unwrap_or(cli.input.with_extension("qoi"));
+            let mut file = File::create(output).expect("cannot create file");
+
+            // encode qoi image and write it to file
             let bytes = qoi_encode(
                 &pixels,
                 QoiDescriptor {
@@ -45,6 +56,7 @@ fn main() {
                 },
             )
             .expect("unable to decode image");
+
             file.write_all(&bytes).expect("unable to write to file");
         }
         None => panic!("no extension"),
