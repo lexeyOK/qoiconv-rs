b0c0a9197f4d347db5f997d8b3916444a493a0a3
lexx <35109763+lexeyOK@users.noreply.github.com>
2023-01-31 23:59:54 +0500

    change file to files , cos it's a vector of paths

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use qoi::*;
 #[derive(Parser)]
 #[command(author,version,about,long_about = None, arg_required_else_help = true)]
 struct Cli {
-    /// Path to input image file
+    /// Path to input image files
     input: Vec<PathBuf>,
     /// Directory to output files *UNIMPLEMENTED*
     #[arg(short = 'd', long = "output-dir")]
